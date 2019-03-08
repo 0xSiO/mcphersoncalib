0cc6ae19904da2294f27ef13a880f42767d4b814
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-03-07 17:58:47 -0800

    Improve calibration plots, add more calibration data
    - Easier to fit the calibration data and guess where peaks are
    - More data added to 300-groove resolvable wavelengths

--- mcphersoncalib.m
+++ mcphersoncalib.m
@@ -3,10 +3,14 @@ function mcphersoncalib(type, grooves, center)
 %   type: 'neon', 'mercury'
 %   grooves: how many grooves in the grating you're using.
 %   center: approx. center wavelength from spectrometer dial.
+% TODO: Make the range used to narrow down possible wavelengths
+% configurable, currently it searches +-256 pixels.
 
 % 512 pixels in SPE snapshot, 
 %   300 groove: 1 pixel = 0.114 nm, actual center shifted left 
-%       ~9.4, 7.83 nm
+%     on average of:
+%     9.4, 7.83, 7.29, 8.14, 7.82, 9.31, 8.68, 7.93, 7.93 nm
+%       (based on several manual tests)
 %   TODO: confirm this! 1200 groove: 1 pixel = 0.69981 nm
 %   TODO: other gratings
 multiplier = 0.114;
@@ -16,6 +20,8 @@ center_wavelength_approx = 1800/grooves * center - offset
 % Range stretches across entire snapshot.
 wavelength_range = [center_wavelength_approx - 256 * multiplier, center_wavelength_approx + 256 * multiplier];
 
+% TODO: currently some locations are missing. See NIST database for more
+% accurate data.
 neon_peaks = [336.99, 341.790, 344.770, 346.658, 347.257, 352.047, 359.353, ...
               533.078, 534.109, 540.056, 585.249, 588.190, 594.483, 597.553, ...
               603.000, 607.434, 609.616, 614.306, 616.359, 621.728, 626.650, ...
@@ -26,8 +32,16 @@ neon_peaks = [336.99, 341.790, 344.770, 346.658, 347.257, 352.047, 359.353, ...
               837.761, 849.536, 863.465, 865.438, 878.062, 878.375, 885.387, ...
               920.176, 1056.241, 1079.804, 1084.448];
 
-neon_peaks_300 = [692.947, 703.241, 717.394, 724.517, 747.244, 794.318, ...
-                  808.246, 813.641, 826.608, 830.033];
+% These are locations of moderate to large peaks that are definitely 
+% resolvable by the 300 groove grating.
+neon_peaks_300 = [692.947, 703.241, 717.394, 724.517, 743.890, 748.890, ...
+                  753.577, 754.404, ...
+                  794.318, 808.246, 811.855, 813.641, 826.608, 830.033, ...
+                  837.761, 841.843, 849.536, 859.126, ...
+                  863.465, 865.438, 868.192, 878.062, 885.387, 886.576, ...
+                  891.950, 914.867, 920.176, ...
+                  922.006, 930.085, 932.651, 942.538, ...
+                  ];
               
 peak_locations = neon_peaks_300; % change depending on grating used
               
--- neon_plots.m
+++ neon_plots.m
@@ -1,16 +1,26 @@
 clc; clf; clear;
 
-center = 120;
-data = readSPE(['300_groove_neon_spectra/', num2str(center), '.spe']);
-plot((1:512)*0.11, data);
-% xlim([0, 512]);   
+center = 155;
+scale = 0.114;
+data = double(readSPE(['300_groove_neon_spectra/', num2str(center), '.spe']));
+plot(scale*(1:512), data);
+xlim([scale, scale*512]);
 
-known_peaks = [794.318, 808.246, 813.641, 826.608, 830.033];
-pixel_peaks = findpeaks_dumb(data, 190, 0);
+known_peaks = [914.867, 920.176, 922.006, 930.085, 932.651, 942.538];
 
-% fit_coeffs = polyfit(pixel_peaks, known_peaks, 1)
-% fit = @(pixels) polyval(fit_coeffs, pixels);
-% 
-% plot(fit(1:512), data);
-% hold on
-% plot(fit(pixel_peaks), data(pixel_peaks), 'rx');
+[peaks, locs] = findpeaks(data, 'MinPeakHeight', 160);
+hold on
+plot(scale*locs, peaks, 'rx');
+
+locs = locs(1:length(known_peaks));
+peaks = peaks(1:length(known_peaks));
+
+fit_coeffs = polyfit(locs, known_peaks, 1)
+fit = @(pixels) polyval(fit_coeffs, pixels);
+percent_error = (known_peaks - fit(locs))./known_peaks * 100
+
+hold off
+plot(fit(1:512), data);
+xlim([fit(1), fit(512)]);
+hold on
+plot(fit(locs), peaks, 'rx');

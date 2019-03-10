e758d9e42780672f55dee666e3b891521d224301
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-03-10 11:21:06 -0700

    Add ability to load calibration spectra in GUI

--- mcphersoncalib_gui.m
+++ mcphersoncalib_gui.m
@@ -1,12 +1,33 @@
-% function mcphersoncalib_gui
-f = figure('Visible', 'on', 'Position', [100, 100, 1200, 800]);
+f = figure('Visible', 'off', 'Position', [100, 100, 1200, 800]);
+f.Name = 'McPherson Calibation';
 
-uicontrol('Style', 'text', 'String', '# of Grooves',  'Position', [10, 30, 80, 25]);
-field_1 = uicontrol('Style', 'edit', 'String', '',    'Position', [15, 10, 70, 25]);
-uicontrol('Style', 'text', 'String', 'Center (nm)',   'Position', [95, 30, 70, 25]);
-field_2 = uicontrol('Style', 'edit', 'String', '',    'Position', [95, 10, 70, 25]);
-fit_btn = uicontrol('Style', 'pushbutton', 'String', 'Try Fit', 'Position', [180, 10, 70, 25]);
+load_btn = uicontrol('Style', 'pushbutton', 'Position', [10, 10, 90, 25], ...
+    'String', 'Load Spectra', 'Callback', @load_spe_file);
+uicontrol('Style', 'text', 'Position', [110, 30, 70, 25], ...
+    'String', '# of Grooves');
+groove_number = uicontrol('Style', 'edit', 'Position', [110, 10, 70, 25]);
+uicontrol('Style', 'text', 'Position', [200, 30, 70, 25], ...
+    'String', 'Center (nm)');
+approx_center = uicontrol('Style', 'edit', 'Position', [200, 10, 70, 25]);
+fit_btn = uicontrol('Style', 'pushbutton', 'Position', [290, 10, 70, 25], ...
+    'String', 'Try Fit');
 
-ha = axes('Units', 'pixels', 'Position', [30,100,1140,680]);
+ha = axes('Units', 'pixels', 'Position', [50,100,1140,680]);
 
-% end
+movegui(f, 'center');
+f.Visible = 'on';
+
+function load_spe_file(obj, event, handles)
+    [file, path] = uigetfile('*.spe');
+    if file == 0
+        return;
+    else
+        setappdata(gcf, 'calibration_data', readSPE(path, file));
+        plot_calibration_data;
+    end
+end
+
+function plot_calibration_data
+    plot(getappdata(gcf, 'calibration_data'));
+    xlim([1, 512]);
+end
